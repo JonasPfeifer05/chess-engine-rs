ab8d137d65633c25bc79c996ef8a95d0ec80e4b6
jonas <jonaspfeifer@drei.at>
2023-05-08 09:18:16 +0200

    added game history

---
+++ src/game_state/game_history.rs
@@ -0,0 +1,31 @@
+use crate::game_board::position::Position;
+
+pub struct GameHistory {
+    played_moves: Vec<(Position, Position)>,
+}
+
+impl Default for GameHistory {
+    fn default() -> Self {
+        Self {
+            played_moves: vec![],
+        }
+    }
+}
+
+impl GameHistory {
+    pub fn push(&mut self, from: Position, to: Position) {
+        self.played_moves.push((from, to));
+    }
+
+    pub fn played_moves(&self) -> &[(Position, Position)] {
+        &self.played_moves
+    }
+
+    pub fn last_n_moves(&self, mut n: usize) -> &[(Position, Position)]  {
+        if self.played_moves.len() < n { n = self.played_moves.len() }
+
+        let index = self.played_moves.len() - n;
+
+        &self.played_moves[index..]
+    }
+}
--- src/game_state/mod.rs
+++ src/game_state/mod.rs
@@ -1,1 +1,2 @@
 pub mod game_state;
+pub mod game_history;
